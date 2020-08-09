26dd0c6b12978e2f858a2d84e5d9e12d76b283c9
fedeemilo <fedeemilo91@gmail.com>
2020-08-08 22:15:46 -0300

    agrego cargar-documentacion, seleccionar-documentacion y error-cargar-documentacion

--- Directo-Paperless/assets/js/main.js
+++ Directo-Paperless/assets/js/main.js
@@ -95,16 +95,16 @@ const fotoPocoClara = (boton, input) => {
 };
 
 // TOGGLE TICK VERDE
-const toggleTick = (botonTick, clase) => {
+const toggleTick = (botonTick) => {
 	for (let i = 0; i < botonTick.length; i++) {
 		botonTick[i].addEventListener('click', () => {
-			botonTick[i].childNodes[1].classList.toggle(clase);
+			botonTick[i].childNodes[1].classList.toggle('tick-active');
 		});
 	}
 };
 
 // FILTRAR BÚSQUEDA
-const filtrarBusqueda = (item, consulta, idx, lista, clase) => {
+const filtrarBusqueda = (item, consulta, idx) => {
 	let contenido = `
 				<li class="list-group-item persona">
 						<span class="persona-nombre"
@@ -121,11 +121,11 @@ const filtrarBusqueda = (item, consulta, idx, lista, clase) => {
 				`;
 
 	if (item.indexOf(consulta) !== -1) {
-		lista.insertAdjacentHTML('afterbegin', contenido);
-		let items = lista.children[0].childNodes[7];
+		listaResultados.insertAdjacentHTML('afterbegin', contenido);
+		let items = listaResultados.children[0].childNodes[7];
 
 		items.onclick = items.addEventListener('click', () => {
-			items.childNodes[1].classList.toggle(clase);
+			items.childNodes[1].classList.toggle('tick-active');
 		});
 	}
 };
@@ -437,12 +437,12 @@ const mensajeError = document.querySelector('.error-sin-resultado');
 if (bodyId == 'validar-identidad') {
 	const resultadosDefault = listaResultados.innerHTML;
 	// Al clickear el tick el mismo hace toggle de color verde
-	toggleTick(botonTick, 'tick-active');
+	toggleTick(botonTick);
 
 	// Al hacer una busqueda se filtran los resultados
 	buscador.addEventListener('input', (e) => {
 		listaResultados.innerHTML = '';
-		toggleTick(botonTick, 'tick-active');
+		toggleTick(botonTick);
 
 		// Si busco por nombre
 		if (isNaN(e.target.value)) {
@@ -450,7 +450,7 @@ if (bodyId == 'validar-identidad') {
 			for (let i = 0; i < personaNombre.length; i++) {
 				let persona = personaNombre[i].textContent.toLowerCase();
 				let personaBuscada = e.target.value.toLowerCase();
-				filtrarBusqueda(persona, personaBuscada, i, listaResultados, 'tick-active');
+				filtrarBusqueda(persona, personaBuscada, i);
 			}
 		}
 
@@ -461,7 +461,7 @@ if (bodyId == 'validar-identidad') {
 				let dniPersona = personaDNI[i].textContent.split(' ')[1];
 				let dniSinPuntos = dniPersona.replace(/\./g, '');
 				let dniBuscado = e.target.value;
-				filtrarBusqueda(dniSinPuntos, dniBuscado, i, 'tick-active');
+				filtrarBusqueda(dniSinPuntos, dniBuscado, i);
 			}
 		}
 
@@ -546,212 +546,5 @@ if (
 }
 
 /* PANTALLAS DE ERROR - fin */
-
-/* ============================================================================= */
-
-/* CARGAR-DOCUMENTACION - inicio */
-
-// VARIABLES
-const botonTickDoc = document.querySelectorAll('.persona-tick');
-const personaNombreDoc = document.querySelectorAll('.persona-nombre');
-const personaDNIDoc = document.querySelectorAll('.persona-dni');
-const buscadorDoc = document.getElementById('buscador');
-const listaResultadosDoc = document.querySelector(
-	'.main-cargar-documentacion__resultados--lista'
-);
-const mensajeErrorOverlayDoc = document.querySelector('.container-overlay');
-const mensajeErrorDoc = document.querySelector('.error-sin-resultado');
-
-if (bodyId === 'cargar-documentacion') {
-	const resultadosDefaultDoc = listaResultadosDoc.innerHTML;
-	// Al clickear el tick el mismo hace toggle de color verde
-	toggleTick(botonTickDoc, 'tick-active-doc');
-
-	// Al hacer una busqueda se filtran los resultados
-	buscadorDoc.addEventListener('input', (e) => {
-		listaResultadosDoc.innerHTML = '';
-		toggleTick(botonTickDoc, 'tick-active-doc');
-
-		// Si busco por nombre
-		if (isNaN(e.target.value)) {
-			// Buscar por nombre de la persona
-			for (let i = 0; i < personaNombreDoc.length; i++) {
-				let personaDoc = personaNombreDoc[i].textContent.toLowerCase();
-				let personaBuscadaDoc = e.target.value.toLowerCase();
-				filtrarBusqueda(personaDoc, personaBuscadaDoc, i, listaResultadosDoc, 'tick-active-doc');
-			}
-		}
-
-		// Si busco por número DNI
-		if (!isNaN(e.target.value)) {
-			// Buscar por DNI de la persona
-			for (let i = 0; i < personaDNIDoc.length; i++) {
-				let dniPersonaDoc = personaDNI[i].textContent.split(' ')[1];
-				let dniSinPuntosDoc = dniPersonaDoc.replace(/\./g, '');
-				let dniBuscadoDoc = e.target.value;
-				filtrarBusqueda(dniSinPuntosDoc, dniBuscadoDoc, i, listaResultadosDoc, 'tick-active-doc');
-			}
-		}
-
-		// Si no hay nada ingresado en el input la lista vuelve a su estado default
-		if (buscadorDoc.value == '') {
-			listaResultadosDoc.innerHTML = '';
-			listaResultadosDoc.insertAdjacentHTML('afterbegin', resultadosDefaultDoc);
-
-			// Añadir función de prender y apagar tick al HTML insertado
-			for (let i = 0; i < personaNombreDoc.length; i++) {
-				let items;
-				i === 0
-					? (items = listaResultadosDoc.children[i].childNodes[7])
-					: (items =
-							listaResultadosDoc.children[i].childNodes[7].previousSibling);
-
-				items.onclick = items.addEventListener('click', () => {
-					items.childNodes[1].classList.toggle('tick-active-doc');
-				});
-			}
-			// Quitar mensaje de 'no hay coincidencias'
-			mensajeErrorDoc.style.display = '';
-		}
-
-		// Si no se encuentra lo que el usuario está buscando
-		if (listaResultadosDoc.innerHTML === '') {
-			let separadores = document.querySelectorAll('hr');
-			for (let i = 0; i < separadores.length; i++) {
-				separadores[i].style.display = 'none';
-			}
-			listaResultados.innerHTML = '';
-			mensajeErrorDoc.style.cssText = `
-
-			position: relative;
-			left: 1.6rem;
-			bottom: .5rem;
-			font-size: .85rem;
-			font-family: 'Roboto', sans-serif;
-			color: #e41f26;
-			display: block !important;
-				
-			`;
-		} else {
-			mensajeErrorDoc.style.display = 'none';
-		}
-	});
-}
-
-/* CARGAR-DOCUMENTACION - fin */
-
-/* ============================================================================= */
-
-/* SELECCIONAR-DOCUMENTACIÓN - inicio */
-
-if (bodyId === 'seleccionar-documentacion' || bodyId === 'error-cargar-documentacion') {
-	const fileSelected = $('.file-upload');
-	const btnRollover = $('.btn-rollover');
-	const containerOverlay = document.querySelector('.container-overlay');
-	const cerrarPopup = document.querySelector('.cerrar-popup');
-	const confirmarCerrar = document.querySelector('.confirmar-cerrar');
-	const trowSelec = document.querySelectorAll('.trow-selec');
-
-	const garbage = $('.garbage');
-	// Chequear si se clickea aceptar o cancel al querer cargar un archivo
-	const checkOnCancel = () => {
-		if (fileSelected.value.length === 0) {
-			console.log('Cancel clicked');
-		} else {
-			console.log('File uploaded: ' + fileSelected.value);
-		}
-		document.body.onfocus = null;
-	};
-
-	const charge = () => {
-		document.body.onfocus = () => {
-			setTimeout(checkOnCancel, 100);
-		};
-	};
-
-	btnRollover.click(function () {
-		if ($(this)[0].parentElement.parentElement.id !== 'boton-todos') {
-			fileSelected
-				.eq($(this)[0].previousElementSibling.id)
-				.trigger('click')
-				.change((e) => {
-					console.log(e.currentTarget.value);
-					if (e.currentTarget.value.length === 0) {
-						$(this).css('background', '#9f9f9f');
-					} else {
-						$(this).css('background', '#23A001');
-					}
-				});
-		}
-	});
-
-	$('.boton-todos').click(function (e) {
-		console.log($(this)[0]);
-
-		fileSelected
-			.eq('0')
-			.trigger('click')
-			.change((e) => {
-				console.log(e.currentTarget.value);
-				if (e.currentTarget.value.length === 0) {
-					btnRollover.css('background', '#9f9f9f');
-					$('.boton-todos .rollover .btn-rollover').css(
-						'background',
-						'#0162c6'
-					);
-				} else {
-					btnRollover.css('background', '#23A001');
-					$('.boton-todos .rollover .btn-rollover').css(
-						'background',
-						'#0162c6'
-					);
-				}
-			});
-	});
-
-	$('.borrar-limpiar').click(function () {
-		btnRollover.css('background', '#9f9f9f');
-		$('.boton-todos').css('background', '#0162c6');
-	});
-
-	// Funcionalidad para los íconos de visualizar (eye)
-	$('.eye-previsualizar').on('click', (e) => {
-		e.preventDefault();
-		Array.from($('.eye-previsualizar img')).forEach(() => {
-			$('.eye-previsualizar img').css('filter', '');
-		});
-
-		$('.eye-previsualizar img')
-			.eq(e.target.alt)
-			.css(
-				'filter',
-				'invert(49%) sepia(74%) saturate(2655%) hue-rotate(74deg) brightness(92%) contrast(99%)'
-			);
-	});
-
-	garbage.on('click', (e) => {
-		e.preventDefault();
-		containerOverlay.classList.remove('d-none');
-	});
-
-	if (bodyId === 'seleccionar-documentacion') {
-		confirmarCerrar.addEventListener('click', (e) => {
-			console.log(confirmarCerrar)
-			console.log(garbage[0])
-		})
-	}
-	
-
-	cerrarPopup.addEventListener('click', (e) => {
-		e.preventDefault();
-		containerOverlay.classList.add('d-none');
-	});
-
-	containerOverlayError.addEventListener('click', () => {
-		containerOverlay.classList.add('d-none');
-	});
-}
-
-/* SELECCIONAR-DOCUMENTACIÓN - fin */
 
 /* ============================================================================= */
